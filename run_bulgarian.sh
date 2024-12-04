aad2907c0649113f42b39236d1e15d2b98516b33
Tom Utting <tom.utting1703@gmail.com>
2024-12-04 19:23:17 +0000

    updated bash script

--- run_bulgarian.sh
+++ run_bulgarian.sh
@@ -10,10 +10,12 @@
 #SBATCH -o slurm.%j.out
 #SBATCH -e slurm.%j.err
 
+#SBATCH --core-spec=0
 #SBATCH --mail-type=ALL 
 #SBATCH --mail-user=u01tu20@abdn.ac.uk 
 
 module load anaconda3
 source activate EvalAlphaEnv
+ulimit -c unlimited
 
 srun python gemma2.py
