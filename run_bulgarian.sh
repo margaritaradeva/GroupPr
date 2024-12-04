afe0a45436b5f7c573e3c115b3fb3742ab8a9c87
Tom Utting <tom.utting1703@gmail.com>
2024-12-04 17:01:42 +0000

    created bash script

---
+++ run_bulgarian.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64G
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu 
+
+#SBATCH -o slurm.%j.out
+#SBATCH -e slurm.%j.err
+
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=u01tu20@abdn.ac.uk 
+
+module load anaconda3
+source activate EvalAlphaEnv
+
+srun python bertscript.py
