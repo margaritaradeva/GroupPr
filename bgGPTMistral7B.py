b468c07b8722d30eddb723134ee09eb003f73c1e
kuba <szachmistrz2@gmail.com>
2024-12-05 23:44:55 +0000

    bye slurm outputs, hi bgGPT

--- bgGPTMistral7B.py
+++ bgGPTMistral7B.py
@@ -15,23 +15,27 @@ def answer_trivia(
     repetition_penalty=1.1
 ):
 
-    device = torch.device("cpu")# if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using Device: {device}")
 
     # Model loading
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         torch_dtype=torch.bfloat16,
-        attn_implementation="eager",
         device_map="auto",
-        low_cpu_mem_usage=True
+        low_cpu_mem_usage=True,
+        use_flash_attn_2=True
     )
+
+    print("Model loaded")
 
  
     tokenizer = AutoTokenizer.from_pretrained(
         model_name,
         use_default_system_prompt=False
     )
+
+    print("Tokenizer loaded")
 
     generation_config = GenerationConfig(
         do_sample=True,
@@ -43,15 +47,17 @@ def answer_trivia(
         eos_token_id=[1, 107]
     )
 
+    print("Generation config loaded")
+
     # Prompt template
-    prompt_template = """<bos><start_of_turn>user
+    prompt_template = """<s>[INST]
         Твоята задача е да отговаряш възможно най-кратко на даденен въпрос. Отговорът, който даваш, трябва да бъде точен, кратък и да съдържа само информация, която отговаря на зададения въпрос. Например:
         Въпрос: Коя е столицата на България?
         Отговор: "София"
         Сега отговори на следния въпрос:
-        Question: {question}<end_of_turn>
-        <start_of_turn>model
+        Question: {question} [/INST] </s>
         """
+
 
     df = pd.read_csv(input_file)
     results = []
@@ -63,11 +69,13 @@ def answer_trivia(
             return answer
         return response.strip()
 
+    # Update the processing section:
     for i in range(0, len(df), batch_size):
+        print("starting ", i)
         batch = df.iloc[i:i+batch_size]
         batch_prompts = [prompt_template.format(question=q) for q in batch['Question']]
         inputs = tokenizer(batch_prompts, return_tensors="pt", padding=True, truncation=True, max_length=512)
-        inputs = {k: v.to(device) for k, v in inputs.items()}
+        inputs = {k: v.to("cuda") for k, v in inputs.items()}
         outputs = model.generate(**inputs, generation_config=generation_config)
         batch_answers = tokenizer.batch_decode(outputs, skip_special_tokens=True)
         clean_answers = [clean_model_response(ans) for ans in batch_answers]
@@ -86,7 +94,7 @@ def answer_trivia(
 if __name__ == "__main__":
     answer_trivia(
         input_file='trivia_qa_bulgarian.csv',
-        output_file='trivia_answers_bgGPTMistral7B.csv',
+        output_file='trivia_answers.csv',
         batch_size=2,
         temperature=0.1,
         max_new_tokens=256
