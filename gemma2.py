9363a5c005fcad8584efef345e20436185be0b74
kuba <szachmistrz2@gmail.com>
2024-12-05 22:44:16 +0000

    parameters fixed

--- gemma2.py
+++ gemma2.py
@@ -6,9 +6,9 @@ import torch.nn as nn
 def answer_trivia(
     input_file,
     output_file,
-    model_name="INSAIT-Institute/BgGPT-Gemma-2-27B-IT-v1.0",
+    model_name="INSAIT-Institute/BgGPT-Gemma-2-9B-IT-v1.0",
     batch_size=10,
-    max_new_tokens=50,
+    max_new_tokens=256,
     temperature=0.1,
     top_k=25,
     top_p=1.0,
@@ -98,5 +98,5 @@ if __name__ == "__main__":
         output_file='trivia_answers.csv',
         batch_size=2,
         temperature=0.1,
-        max_new_tokens=50
+        max_new_tokens=256
     )
