9e45e6bb0e0c56909541455e0eedf482920dffb0
kuba <szachmistrz2@gmail.com>
2024-12-05 16:38:55 +0000

    new script for my device

---
+++ run_bulgarian_kuba.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu 
+
+#SBATCH -o slurm.%j.out
+#SBATCH -e slurm.%j.err
+
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=u14jp20@abdn.ac.uk 
+
+module load miniconda3
+source activate eval_venv
+
+srun python gemma2.py --export=ALL
