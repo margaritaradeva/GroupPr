a53766e40fce5683643b39c6e16e38fd15f8cccc
kuba <szachmistrz2@gmail.com>
2024-12-06 00:05:53 +0000

    gen config removed

--- bgGPTMistral7B.py
+++ bgGPTMistral7B.py
@@ -76,7 +76,7 @@ def answer_trivia(
         batch_prompts = [prompt_template.format(question=q) for q in batch['Question']]
         inputs = tokenizer(batch_prompts, return_tensors="pt", padding=True, truncation=True, max_length=512)
         inputs = {k: v.to("cuda") for k, v in inputs.items()}
-        outputs = model.generate(**inputs, generation_config=generation_config)
+        outputs = model.generate(**inputs)
         batch_answers = tokenizer.batch_decode(outputs, skip_special_tokens=True)
         clean_answers = [clean_model_response(ans) for ans in batch_answers]
         
